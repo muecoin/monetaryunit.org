c8016d9f62ff3ad860f49e8dad1e153b6adcf9aa
Sotiris Blad <sotiris2009@hotmail.com>
2019-01-09 16:59:59 +0200

    Add files via upload

---
+++ buy.php
@@ -0,0 +1,55 @@
+<center><h1>Exchanges:</h1></center>
+<center><a href="https://bittrex.com/Market/Index?MarketName=BTC-MUE" target="_blank"><img src="https://investorshub.advfn.com/uimage/uploads/2018/3/20/vtbfkbittrex.png" height="20%"></img></a>
+<a href="https://upbit.com/exchange?code=CRIX.UPBIT.BTC-MUE" target="_blank"><img src="https://s2.coinmarketcap.com/static/img/exchanges/200x200/351.png" height="20%"></img></a>
+<a href="https://wallet.crypto-bridge.org/market/BRIDGE.MUE_BRIDGE.BTC" target="_blank"><img src="https://steemitimages.com/DQmakmLJRqucg1vt9RGa6xRDDEE7mWLyorUZwC5dBKGqEik/image.png" height="20%"></img></a>
+<a href="https://bittylicious.com/" target="_blank"><img src="https://pbs.twimg.com/profile_images/378800000015551093/e807938d0dc2cbe5a109aa75207b73d5_400x400.png" height="20%"></img></a></center>
+<link rel="shortcut icon" href="images/mue-favicon.ico?crc=461228135"/>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+<div id='cryptowolf'/>
+
+<!--<script>
+
+var external = 'mue';
+
+var cryptowolf = 'https://external.cryptowolf.eu/v3/';
+
+function defer(method) {
+
+   if (window.jQuery) {
+
+       method();
+
+   } else {
+
+       setTimeout(function() { defer(method) }, 50);
+
+   }
+
+}
+
+defer(function () {
+
+ jQuery.getScript("https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js")
+
+  .done(function() {
+
+    $("#cryptowolf").load(cryptowolf + 'ext.html');
+
+    $.getScript(cryptowolf + 'js/cryptowolf.js');
+
+ })
+
+});
+
+</script>-->
+<center><iframe id="foo" width="500" height="520" src="https://coinswitch.co/widget?widget_id=d3e8895f&from=btc&to=mue&amount=0.01" frameborder="0" scrolling="auto" style="overflow-y: hidden;border: 1px solid #aaa;border-top: none;">Can't load widget</iframe></center>
+<script>
+$(document).ready(function(){
+    $('#foo').ready(function(){
+        $('#foo').contents().find('body').alert('Hey, i`ve changed content of <body>! Yay!!!');
+    });
+});
+
+</script>
